8fa79481e9fdcd5221474622297206f012533c83
Robotics 2207 <Robotics 2207>
2019-12-05 21:45:45 -0700

    Added inputs for arms, made controller static

--- src/main/java/frc/robot/subsystems/ArmShoulder.java
+++ src/main/java/frc/robot/subsystems/ArmShoulder.java
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -7,17 +7,36 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
 import edu.wpi.first.wpilibj.command.Subsystem;
+import frc.robot.RobotMap;
 
 /**
- * This represents the robot Arm.
+ * Add your docs here.
  */
-public class Arm extends Subsystem {
+public class ArmShoulder extends Subsystem {
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
+  WPI_TalonSRX motor = new WPI_TalonSRX(RobotMap.armMotors.shoulderMotor);
   @Override
   public void initDefaultCommand() {
     // Set the default command for a subsystem here.
     // setDefaultCommand(new MySpecialCommand());
+  }
+  /**
+   * This is a method to move up the Arm's elbow.
+   * @param speed
+   * This parameter is a value between -1.0 and 1.0
+   */
+  //TODO: Make this use a freaking actual sensor, and hook it up to Arm Subsys.
+  public void moveUp(double speed) {
+    motor.set(speed);
+  }
+  public void moveDown(double speed){
+    motor.set(-speed);
+  }
+  public void stopMoving(){
+    motor.set(0);
   }
 }
